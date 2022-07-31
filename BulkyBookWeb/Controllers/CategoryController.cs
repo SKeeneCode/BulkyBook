f5498e8d419b02ad44e2044f8de60da1527f7bee
Sam <samkeene@protonmail.com>
2022-07-31 15:26:38 +0100

    create category

--- BulkyBookWeb/Controllers/CategoryController.cs
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -15,5 +15,16 @@ namespace BulkyBookWeb.Controllers {
             IEnumerable<Category> objCategoryList = _db.Categories;
             return View(objCategoryList);
         }
+
+        public IActionResult Create() {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj) {
+            _db.Categories.Add(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
 }
